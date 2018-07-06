b81e231ad076d2681df44561a5581c3f0b373e80
岡田徹 <toruhandbell0424@gmail.com>
2018-07-06 21:57:00 +0900

    Hello World

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
-import rootReducer from './reducers'
-import App from './components/App'
-
-const store = createStore(rootReducer)
+import App from './App'
 
 render(
-    <Provider store={store}>
+    <Provider>
         <App />
     </Provider>,
     document.getElementById('root')
