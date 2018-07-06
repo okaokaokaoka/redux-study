e75675da432292dadacbbb0fed74d00d51b998f4
岡田徹 <toruhandbell0424@gmail.com>
2018-07-07 02:12:04 +0900

    Complete Todolist

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-import App from './App'
+import { createStore } from 'redux'
+import todo from './reducers'
+import { addTodo } from './actions';
+import App from './components/App'
+
+let store = createStore(todo)
 
 render(
-    <Provider>
+    <Provider store={store}>
         <App />
     </Provider>,
     document.getElementById('root')
